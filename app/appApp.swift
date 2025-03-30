5f65b89f50aaccf83d186f29dcf878ff028f6f0e
Sumanth Kanakala <s.kanakala@epilot.cloud>
2025-03-30 22:09:34 +0530

    Initial Commit

---
+++ app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Sumanth Kanakala on 30/03/25.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
